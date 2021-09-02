62449fac3f35a2bbc1a6b6c04052ae65e2c7124c
JonkiTroll <jonr16@ru.is>
2021-09-02 09:54:42 +0000

    added interrupt routine

--- Group_project_1/src/main.cpp
+++ Group_project_1/src/main.cpp
@@ -5,9 +5,49 @@
 #include "digital_in.h"
 #include "digital_out.h"
 
+#include <avr/interrupt.h>
+
+Digital_out led(5);
+bool flag = false;
+int counter = 0;
+
+ISR(INT0_vect){
+    if(!flag){
+        flag = true;
+        counter++;
+        led.set_hi();
+    }
+}
+
+ISR(INT1_vect){
+    if(!flag){
+        flag = true;
+        counter--;
+        led.set_lo();
+    }
+}
 
 int main() {
 
+    EICRA |= (1<<ISC11);
+
+    EIMSK |= ((1 << INT0) | (1<<INT1));
+
+    DDRD &= ~((1 << DDD2) | (1 << DDD1));
+
+    PORTB |= ((1 << PIN2) | (1 << PIN1));
+
+    sei();
+
+    while(true){
+        if(!(PIND & (1<<PIN1)) && !(PIND & (1 << PIN2))){
+            flag = false;
+        }
+    }
+
+
+
+    /*
     Digital_in encoderA(0);
     Digital_in encoderB(1);
     Digital_out led(5);
@@ -33,15 +73,9 @@ int main() {
             flag = false;
             led.set_lo();
         }
-            /*
-        if(increment >= 120){
-            increment = 0;
-            led.toggle();
 
-        }
-             */
     }
-
+*/
 
     return 0;
 
