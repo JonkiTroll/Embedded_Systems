a4fd470f7cd34413bac4ab0d6bd9953f896ff13d
Jóhannes Bergur Gunnarsson <joibg@simnet.is>
2021-09-04 14:22:25 +0000

    deleted old comments

--- Group_project_1/src/main.cpp
+++ Group_project_1/src/main.cpp
@@ -60,36 +60,6 @@ int main() {
 
 
 
-    /*
-    Digital_in encoderA(0);
-    Digital_in encoderB(1);
-    Digital_out led(5);
-
-    int8_t increment = 0;
-    bool flag = false;
-
-    encoderA.init();
-    encoderB.init();
-    led.init();
-
-    while(true){
-
-        if(encoderA.is_hi() && !flag){
-            increment++;
-            flag = true;
-            led.set_hi();
-        } else if (encoderB.is_hi() && !flag) {
-            increment--;
-            flag = true;
-            led.set_hi();
-        } else if (encoderA.is_lo() && encoderB.is_lo()) {
-            flag = false;
-            led.set_lo();
-        }
-
-    }
-*/
-
     return 0;
 
 }
