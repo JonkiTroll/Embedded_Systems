c0eaffab81f50d4fa1143fe322702836612bce11
JonkiTroll <jonr16@ru.is>
2021-09-14 12:24:54 +0000

    Created group project 2

---
+++ Group_project_2/src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by group on 8/18/21.
+//
+//
+#include <util/delay.h>
+#include "digital_in.h"
+#include "digital_out.h"
+
+#include <avr/interrupt.h>
+
+int main() {
+
+
+    return 0;
+
+}
